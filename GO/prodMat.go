e4f3c12e3d228bfbe3e36c4c9bf9aedc1a9dfb2d
Louise726 <louisemarc726@yahoo.com>
2023-12-21 10:40:36 +0100

    première version de la fonction multiplication de matrices avec une fonction main qui test la fonction MatProd

---
+++ GO/prodMat.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const taille = 3
+
+func ProdMat(taille int, A [taille][taille]int, B [taille][taille]int) (C [taille][taille]int) {
+	for i := 0; i < taille; i++ {
+		for j := 0; j < taille; j++ {
+			C[i][j] = 0
+			for k := 0; k < taille; k++ {
+				C[i][j] = C[i][j] + A[i][k]*B[k][j]
+			}
+		}
+	}
+	return
+}
+
+func main() {
+	var matA [taille][taille]int
+	var matB [taille][taille]int
+	var matC [taille][taille]int
+
+	for i := 0; i < taille; i++ {
+		for j := 0; j < taille; j++ {
+			matA[i][j] = 2
+			matB[i][j] = 1
+			matC[i][j] = 1
+		}
+	}
+
+	matC = prodMat(taille, matA, matB)
+	fmt.Println(matC)
+}
