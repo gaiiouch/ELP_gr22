70c69741d63a5a1907ccfba9c710cc50002c8398
gaiiouch <ag.mauger@gmail.com>
2024-01-14 15:23:11 +0100

    Essais sur serveur et client TCP. NE FONCTIONNE PAS.

---
+++ GO/tcpclient.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8000")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for i := 0; i < 5; i++ {
+		io.WriteString(conn, fmt.Sprintf("Coucou %d\n", i))
+	}
+
+}
--- GO/tcpserver.go
+++ GO/tcpserver.go
@@ -6,38 +6,36 @@ import (
 	"net"
 )
 
-func main(){
-	//listen 
-	//accept 
-	//handle connection with threads 
+func handle(conn net.Conn) {
+	defer conn.Close()
 
-	dstream ,err := net.Listen("tcp", ":8000")
-
-	if err != nil {
-		fmt.Println(err)
-		return 
-	}
-
-	defer dstream.Close()
-
-	for{
-		con,err := dstream.Accept()
-		if err != nil {
-			fmt.Println(err)
-			return 
-		}
-		go handle(con)
-	}
-}
-
-func handle(con net.Conn) {
 	for {
-		data ,err := bufio.NewReader(con).ReadString('\n') //read the data only string 
+		data, err := bufio.NewReader(conn).ReadString('\n') //read the data only string
 		if err != nil {
 			fmt.Println(err)
-			return 
+			return
 		}
 		fmt.Println(data)
 	}
-	con.Close()
+}
+
+func main() {
+
+	ln, err := net.Listen("tcp", ":8000") // écoute sur le port 8000
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		go handle(conn)
+	}
 }
