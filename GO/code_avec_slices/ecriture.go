a39097bfc251a55b5ffeea00f731b98b2bb99944
gaiiouch <ag.mauger@gmail.com>
2024-01-19 11:21:07 +0100

    Nettoyage des fonctions inutiles et meilleure organisation

--- GO/code_avec_slices/ecriture.go
+++ GO/code_avec_slices/ecriture.go
@@ -5,33 +5,8 @@ import (
 	"os"
 )
 
-// FONCTION INUTILE
-func EcritureMatString(n int, mat []string, filename string) error {
-
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// ecriture dans le file
-	for i := 0; i < n; i++ {
-		_, err = fmt.Fprintf(file, "%s ", mat[i])
-		if err != nil {
-			return err
-		}
-
-		_, err = fmt.Fprintf(file, "\n")
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 // écrit une matrice dans un fichier à partir d'une variable matrice déjà créée
-func EcritureMatInt(n int, mat [][]int, filename string) error {
+func EcritureMat(n int, mat [][]int, filename string) error {
 
 	file, err := os.Create(filename)
 	if err != nil {
