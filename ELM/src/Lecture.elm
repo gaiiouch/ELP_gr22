f49c6753ac5105e28dc38cfeb8eb605e2608136b
gaiiouch <ag.mauger@gmail.com>
2024-01-20 15:20:46 +0100

    Début de création application. PROBLEME SUR URL A REGLER

---
+++ ELM/src/Lecture.elm
@@ -0,0 +1,90 @@
+module Lecture exposing (..)
+
+-- Make a GET request to load a text file ("thousand_words_things_explainer.txt")
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/effects/http.html
+--
+
+import Browser
+import Html exposing (Html, text, pre)
+import Http
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+
+-- MODEL
+
+
+type Model
+  = Failure
+  | Loading
+  | Success String
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Loading
+  , Http.get
+      { url = "https://perso.liris.cnrs.fr/tristan.roussillon/GuessIt/thousand_words_things_explainer.txt"
+      , expect = Http.expectString GotText
+      }
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = GotText (Result Http.Error String)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    GotText result ->
+      case result of
+        Ok fullText ->
+          (Success fullText, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  case model of
+    Failure ->
+      text "I was unable to load the text file."
+
+    Loading ->
+      text "Loading..."
+
+    Success fullText ->
+      pre [] [ text fullText ]
