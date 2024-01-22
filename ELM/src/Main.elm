300c3290f2df5b66388856ed799b95108567aedc
gaiiouch <ag.mauger@gmail.com>
2024-01-22 23:41:59 +0100

    Amélioration de l'affichage

--- ELM/src/Main.elm
+++ ELM/src/Main.elm
@@ -7,14 +7,13 @@ module Main exposing (..)
 
 
 import Browser
-import Html exposing (Html, text, pre, div, input, button)
+import Html exposing (Html, text, blockquote, pre, div, input, button)
 import Html.Events exposing (onInput, onClick)
 import Html.Attributes exposing (placeholder, value, style)
 import Http
 import Random
 import List.Extra exposing (getAt)
 import Json.Decode exposing (Decoder, map2, field, string, list)
-import Json.Decode exposing (Error(..))
 
 
 -- MAIN
@@ -173,16 +172,16 @@ view : Model -> Html Msg
 view model =
   case model.state of
     Loading ->
-      div [style "font-family" "Noto Sans, sans-serif"] [text "Loading..."]
+      blockquote [style "font-family" "Noto Sans, sans-serif"] [text "Loading..."]
     FailureText ->
-      div [style "font-family" "Noto Sans, sans-serif"] [text "I was unable to load the text file."]
+      blockquote [style "font-family" "Noto Sans, sans-serif"] [ div [] [text "I was unable to load the text file."]]
     FailureDef ->
-      div [style "font-family" "Noto Sans, sans-serif"] [text "I was unable to load the definition of the word."]
+      blockquote [style "font-family" "Noto Sans, sans-serif"] [ div [] [text "I was unable to load the definition of the word."]]
     Success -> 
-      div [style "font-family" "Noto Sans, sans-serif"]
+      blockquote [style "font-family" "Noto Sans, sans-serif"]
             [ viewShowAnswer model
             , div [style "font-size" "20px"] [text "Guess the word according to its definition : \n"]
-            , pre [style "font-family" "Noto Sans, sans-serif"] (recur1 model.def)
+            , pre [style "font-family" "Noto Sans, sans-serif", style "font-size" "15px"] (recur1 model.def)
             , input [ placeholder "Type a word", value model.userInput, onInput Change ] []
             , button [ onClick Erase ] [ text "Erase" ]
             , button [ onClick Check ] [ text "Show the answer" ]
@@ -202,7 +201,7 @@ viewValidation : Model -> Html msg
 viewValidation model =
     if model.userInput == model.answer then
         div [ style "color" "green" ] [ text ("Correct answer ! The word was indeed " ++ model.answer ++ " !") ]
-    else if String.length model.userInput > 0 then
+    else if String.length model.userInput > 0 && model.isChecked == False then
         div [ style "color" "red" ] [ text ("Wrong answer ! The word is not " ++ model.userInput ++ ".") ]
     else if model.isChecked == True then
         div [ style "color" "black" ] [ text ("The right answer was " ++ model.answer ++ ".") ]
@@ -214,18 +213,18 @@ recur1 : List Def -> List (Html Msg)
 recur1 list =
     case list of 
         [] -> []
-        (x :: xs) -> [ text ("Meaning :\n") ] ++ recur2(x.meanings) ++ recur1(xs)
+        (x :: xs) -> [ div [style "font-weight" "bold"] [text ("Meaning :\n")] ] ++ recur2(x.meanings) ++ recur1(xs)
 
 
 recur2 : List Meaning -> List (Html Msg)
 recur2  list =
     case list of 
         [] -> []
-        (x :: xs) -> [ text ("  " ++ x.partOfSpeech ++ "\n") ] ++ recur3(x.definitions) ++ recur2(xs)
+        (x :: xs) -> [ div [style "font-style" "italic"] [text ("  " ++ x.partOfSpeech ++ "\n") ]] ++ recur3(x.definitions) ++ recur2(xs)
 
 
 recur3 : List String -> List (Html Msg)
 recur3  list =
     case list of 
-        [] -> []
+        [] -> [text ("\n")]
         (x :: xs) -> [text ("       - " ++ x ++ "\n")] ++ recur3(xs)
