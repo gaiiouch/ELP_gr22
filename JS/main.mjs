572a0b030f7090597213899da6794c7ec8f97ebf
Louise726 <louisemarc726@yahoo.com>
2024-01-31 16:24:06 +0100

    l'option jarnac fonctionne. La fin du jeu a lieu quand un joueur a 8 mots. Le compte des points est bon. Le jeu est fonctionnel.

--- JS/main.mjs
+++ JS/main.mjs
@@ -1,21 +1,11 @@
 import random from 'random'
 import inquirer from 'inquirer'
 
-//github de la pizza : https://github.com/SBoudrias/Inquirer.js/tree/master/packages/inquirer/examples
-
 //INITIALISATION DU JEU
-
-// 14 A, 4 B, 7 C, 5 D, 19 E, 2 F, 4 G, 2 H, 11 I, 1 J, 1 K, 6 L, 5 M, 9 N, 8 O, 4 P, 1 Q, 10 R, 7 S, 9 T, 8 U, 2 V, 1 W, 1 X, 1 Y et 2 Z.
 let sac =  [["A", 14],["B", 4],["C", 7],["D", 5],["E", 19],["F", 2],["G", 4],["H", 2],["I", 11],["J", 1],["K", 1],["L", 6],["M", 5],["N" , 9],["O" , 8],["P" , 4],["Q" , 1],["R" , 10],["S", 7],["T", 9],["U", 8],["V", 2],["W" , 1],["X" , 1],["Y" , 1],["Z", 2]]
 
-//let tapis1 = [["B","O","N"],["J","E","U","X"]]
-//let tapis2 = [["H","E","L","L","O"]]
 let tapis1 = []
 let tapis2 = []
-
-//pas sûre que ce soit utile
-//let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
 
 //FONCTIONS DU JEU
 const piocher_x_lettres = function(x, sac) {
@@ -86,10 +76,6 @@ const playGame = async () => {
             ];
 
             let play_jarnac = await inquirer.prompt(jarnac).then((answers) => {
-                // récapitulation de la réponse donnée par le joueur et on l'affiche,
-                // mise à jour du tapis et de la main du joueur
-                //console.log('\nPlaying Jarnac');
-                //console.log(JSON.stringify(answers, null, '  '));
                 let play_jarnac = answers["Jarnac"]
                 return play_jarnac
             });
@@ -124,10 +110,6 @@ const playGame = async () => {
                 ];
 
                 let chosen_lign = await inquirer.prompt(question_lign).then((answers) => {
-                    // récapitulation de la réponse donnée par le joueur et on l'affiche,
-                    // mise à jour du tapis et de la main du joueur
-                    //console.log('\nLign number');
-                    //console.log(JSON.stringify(answers, null, '  '));
                     let chosen_lign = answers["lign"]
                     return chosen_lign
                 });
@@ -154,9 +136,6 @@ const playGame = async () => {
                 let letters = []
                 while (!end_word) {
                     letters = await inquirer.prompt(question_letter).then((answers) => {
-                        // récapitulation de la réponse donnée par le joueur et on l'affiche,
-                        // mise à jour du tapis et de la main du joueur
-
                         if (answers["letter"] === "end word") {
                             end_word = true
                             //console.log('\nTurn summary:');
@@ -181,7 +160,6 @@ const playGame = async () => {
                 
                 tapis.push([]);
                 for (let i = 0; i < letters.length; i++){
-                    //poser_lettre(letters[i], tapis1, chosen_lign-1, tapis1[chosen_lign-1].length - 1); 
                     tapis[tapis.length-1].push(letters[i])
                 }
 
@@ -223,10 +201,6 @@ const playGame = async () => {
 
         // Use await to wait for player input before moving on
         let chosen_lign = await inquirer.prompt(question_lign).then((answers) => {
-            // récapitulation de la réponse donnée par le joueur et on l'affiche,
-            // mise à jour du tapis et de la main du joueur
-            //console.log('\nLign number');
-            //console.log(JSON.stringify(answers, null, '  '));
             let chosen_lign = answers["lign"]
             return chosen_lign
         });
@@ -254,13 +228,8 @@ const playGame = async () => {
         let letters = []
         while (!end_word) {
             letters = await inquirer.prompt(question_letter).then((answers) => {
-                // récapitulation de la réponse donnée par le joueur et on l'affiche,
-                // mise à jour du tapis et de la main du joueur
-
                 if (answers["letter"] === "end word") {
                     end_word = true
-                    //console.log('\nTurn summary:');
-                    //console.log(JSON.stringify(letters, null, '  '));
                 } else {
                     letters.push(answers['letter'])
                     let index = main.indexOf(answers['letter']);
@@ -278,7 +247,6 @@ const playGame = async () => {
         }
 
         for (let i = 0; i < letters.length; i++){
-            //poser_lettre(letters[i], tapis1, chosen_lign-1, tapis1[chosen_lign-1].length - 1); 
             tapis[chosen_lign-1].push(letters[i])
         }
 
