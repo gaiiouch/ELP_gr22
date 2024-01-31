f0830b8a493a8009ab924c3ea5a51224078fabc9
gaiiouch <ag.mauger@gmail.com>
2024-01-31 15:31:05 +0100

    Jeu tourne. A peaufiner pour faire une partie complète et compter les points

--- JS/main.mjs
+++ JS/main.mjs
@@ -14,7 +14,7 @@ let tapis1 = []
 let tapis2 = []
 
 //pas sûre que ce soit utile
-let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+//let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 
 //FONCTIONS DU JEU
@@ -40,9 +40,9 @@ const affiche_tapis = function(tapis, num_joueur) {
     }
 }
 
+//------------------------------------------------------------------------------------------------------------------------
+
 //DEBUT DU JEU
-let jarnac_possible = true
-let play_jarnac = 'No'
 let main1 = piocher_x_lettres(6, sac)
 let main2 = piocher_x_lettres(6, sac)
 main1.push("end word")
@@ -55,17 +55,23 @@ const playGame = async () => {
     let main;
 
     while (!end) {
-        if (tour % 2 === 0){
-            tapis = tapis1
-            main = main1
-            console.log("Tour du joueur " + tour % 2)
-        } else {
-            tapis = tapis2
-            main = main2
-            console.log("Tour du joueur " + tour % 2)
-        }
 
-        if ((tour > 1) && (jarnac_possible === true)){
+        console.log("Tour du joueur " + (tour % 2 + 1))
+
+        console.log("main du joueur 1")
+        console.log(main1)
+        console.log("main du joueur 2")
+        console.log(main2)
+        affiche_tapis(tapis1, 1);
+        affiche_tapis(tapis2, 2);
+
+        // DEBUT TOUR
+        if (tour > 0) {
+
+            console.log()
+
+
+
             let jarnac = [
                 {
                     type : 'list',
@@ -81,7 +87,7 @@ const playGame = async () => {
             let play_jarnac = await inquirer.prompt(jarnac).then((answers) => {
                 // récapitulation de la réponse donnée par le joueur et on l'affiche,
                 // mise à jour du tapis et de la main du joueur
-                console.log('\nLign number');
+                console.log('\nPlaying Jarnac');
                 console.log(JSON.stringify(answers, null, '  '));
                 let play_jarnac = answers["Jarnac"]
                 return play_jarnac
@@ -97,12 +103,105 @@ const playGame = async () => {
                     main = main1
                     console.log("Coup de Jarnac du joueur 2 sur le joueur 1 !")
                 }
+
+                console.log("main de l'autre joueur ")
+                console.log(main)
+                affiche_tapis(tapis1, 1);
+                affiche_tapis(tapis2, 2);
+
+                let question_lign = [
+                    {
+                        type : 'list',
+                        name : 'lign',
+                        message : 'On which lign do you want to make changes ?',
+                        choices: Array.from({ length: tapis.length}, (_, index) => index + 1), //les choix sont les lignes déjà existantes ou écrire une nouvelle ligne
+                        lign_choice(val) {
+                            return val;
+                        },
+                    },
+                ];
+
+                let chosen_lign = await inquirer.prompt(question_lign).then((answers) => {
+                    // récapitulation de la réponse donnée par le joueur et on l'affiche,
+                    // mise à jour du tapis et de la main du joueur
+                    console.log('\nLign number');
+                    console.log(JSON.stringify(answers, null, '  '));
+                    let chosen_lign = answers["lign"]
+                    return chosen_lign
+                });
+
+                main.pop()
+                main = main.concat(tapis[chosen_lign-1])
+                main.push("end word")
+                console.log(main)
+                tapis.splice(chosen_lign-1, 1)
+
+                let question_letter = [
+                    {
+                        type : 'list',
+                        name : 'letter',
+                        message : 'Write your word letter per letter :',
+                        choices: main, //main du joueur + lettres déjà sur la ligne
+                        filter(val) {
+                            return val;
+                        },
+                    },
+                ];
+
+                let end_word = false
+                let letters = []
+                while (!end_word) {
+                    letters = await inquirer.prompt(question_letter).then((answers) => {
+                        // récapitulation de la réponse donnée par le joueur et on l'affiche,
+                        // mise à jour du tapis et de la main du joueur
+
+                        if (answers["letter"] === "end word") {
+                            end_word = true
+                            console.log('\nTurn summary:');
+                            console.log(JSON.stringify(letters, null, '  '));
+                        } else {
+                            letters.push(answers['letter'])
+                            let index = main.indexOf(answers['letter']);
+                            if (index !== -1) {
+                                main.splice(index, 1);
+                            }
+                        }
+                        return letters
+                    });
+                    
+                }
+
+                if (tour % 2 == 0) {
+                    tapis = tapis1
+                } else {
+                    tapis = tapis2
+                }
+                
+                tapis.push([]);
+                for (let i = 0; i < letters.length; i++){
+                    //poser_lettre(letters[i], tapis1, chosen_lign-1, tapis1[chosen_lign-1].length - 1); 
+                    tapis[tapis.length-1].push(letters[i])
+                }
+
             }
 
-            jarnac_possible = false
         }
 
-        console.log("main du joueur " + tour%2)
+        if (tour % 2 == 0) {
+            tapis = tapis1
+            main = main1
+        } else {
+            tapis = tapis2
+            main = main2
+        }
+
+        if (tour > 1) {
+            main.pop()
+            main = main.concat(piocher_x_lettres(1, sac))
+            main.push("end word")
+        }
+
+        console.log("main du joueur")
         console.log(main)
         affiche_tapis(tapis1, 1);
         affiche_tapis(tapis2, 2);
@@ -118,7 +217,7 @@ const playGame = async () => {
                 },
             },
         ];
-        
+
         // Use await to wait for player input before moving on
         let chosen_lign = await inquirer.prompt(question_lign).then((answers) => {
             // récapitulation de la réponse donnée par le joueur et on l'affiche,
@@ -145,8 +244,7 @@ const playGame = async () => {
                 },
             },
         ];
-        
-        console.log(main)
+
         let end_word = false
         let letters = []
         while (!end_word) {
@@ -179,25 +277,16 @@ const playGame = async () => {
             tapis[chosen_lign-1].push(letters[i])
         }
 
-        main.pop()
-        main = main.concat(piocher_x_lettres(6-main.length, sac))
-        main.push("end word")
         if (tour % 2 === 0){
             main1 = main
-            console.log("Fin du tour du joueur " + tour%2)
+            console.log("Fin du tour du joueur " + (tour%2+1))
         } else {
             main2 = main
-            console.log("Fin du tour du joueur " + tour%2)
+            console.log("Fin du tour du joueur " + (tour%2+1))
         }
 
-        if (play_jarnac === 'No' || tour < 1){
-            tour ++;
-            jarnac_possible = true;
-        }
-        if (play_jarnac === 'Yes') {
-            jarnac_possible = false;
-        }
-        
+        tour ++
+
         if (tour == 4) {
             end = true;
             affiche_tapis(tapis1, 1);
